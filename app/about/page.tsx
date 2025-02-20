e454cca82c8358cbbdd3b0224e4678c2b1263c6c
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-02-20 17:41:42 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -26,7 +26,7 @@ export default function About() {
   return (
     <>
       <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;600&display=swap" rel="stylesheet" />
-      <div className="mt-[135px] sm:mt-[165px] max-w-[1280px] mx-auto mb-12">
+      <div className="mt-[135px] sm:mt-[165px] max-w-[1280px] mx-8 sm:mx-auto mb-12">
         <p className="text-[14px] text-gray-700 mt-4">
           <span className="font-[urbanist] text-[#ccc] hover:text-[#035C7A]">
             <Link href="/" passHref>Home</Link>
