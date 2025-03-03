d1923d80474a496b6e3a067aade625d8c25ee388
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-03-03 13:25:06 +0530

    Update CanonicalURL.tsx

--- components/CanonicalURL.tsx
+++ components/CanonicalURL.tsx
@@ -1,1 +1,12 @@
+import { useRouter } from 'next/router';
 
+export default function CanonicalURL() {
+  const siteUrl = 'https://www.eazetours.com/';
+  const { asPath } = useRouter();
+  const cleanPath = asPath.split('#')[0].split('?')[0];
+  const canonicalUrl = `${siteUrl}` + (router.asPath === '/' ? '' : cleanPath);
+
+  return (
+    <link rel="canonical" href={canonicalUrl} />
+  );
+};
