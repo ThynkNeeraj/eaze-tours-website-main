269b1aede187a2e4553ad41ed975b57a5d819bf6
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-03-04 14:03:29 +0530

    Update VerticalTimeline.tsx

--- components/VerticalTimeline.tsx
+++ components/VerticalTimeline.tsx
@@ -123,27 +123,7 @@ function VerticalTimeline({ tourPackage }: VerticalTimelineProp) {
                     </div>
                 </div>
 
-                <div className="overflow-auto border-0 sm:border-2 border-[#6e9753] mx-8 rounded-[20px] p-5">
-                    <h2 className="text-2xl font-bold text-black mb-4">Enquiry Form</h2>
-                    <form id="enquiry-form" onSubmit={onSubmit}>
-                        <div className="mb-4">
-                            <input type="text" name="name" placeholder="Name" className="input input-bordered w-full" value={enquiryName} onChange={(e) => setEnquiryName(e.target.value)} required />
-                        </div>
-                        <div className="mb-4">
-                            <input type="email" name="email" placeholder="Email" className="input input-bordered w-full" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                        </div>
-                        <div className="mb-4">
-                            <input type="text" name="subject" placeholder="Subject" className="input input-bordered w-full" value={subject} onChange={(e) => setSubject(e.target.value)} required />
-                        </div>
-                        <div className="mb-4">
-                            <textarea name="message" placeholder="Message" className="textarea textarea-bordered w-full" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary w-full" type="submit">Send</button>
-                        </div>
-                    </form>
-                    <div className={`mt-3 text-green-600 text-center ${showToast}`}>Your enquiry has been sent successfully!</div>
-                </div>
+
             </div>
 
             <div className="my-12 max-w-screen-xl mx-8">
