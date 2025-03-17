1e2de299809c9e63469369c271efb4d18c84c8ee
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-03-17 18:24:07 +0530

    Update VerticalTimelineElement.tsx

--- components/VerticalTimelineElement.tsx
+++ components/VerticalTimelineElement.tsx
@@ -7,17 +7,17 @@ interface VerticalTimelineElementProp {
 
 function VerticalTimelineElement({ time, title, description, isLast }: VerticalTimelineElementProp) {
     const timelineClass = parseInt(time) % 2 === 1
-        ? "timeline-end md:text-start mb-[70px] ml-[-15px] mr-5 sm:ml-5 sm:mr-0 mt-5"
-        : "timeline-end md:text-start mb-[70px] ml-[-15px] mr-5 sm:ml-5 sm:mr-0 mt-5";
+        ? "timeline-end md:text-start mb-[70px] ml-[-5px] mr-5 sm:ml-5 sm:mr-0 mt-2 sm:mt-5"
+        : "timeline-end md:text-start mb-[70px] ml-[-5px] mr-5 sm:ml-5 sm:mr-0 mt-2 sm:mt-5";
 
     return (
         <>
-            <div className="timeline-middle text-[18px] text-[#6E9753] w-[50px] h-[50px] border-2 border-[#6E9753] rounded-full flex items-center justify-center">
+            <div className="timeline-middle text-[12px] sm:text-[18px] text-[#6E9753] w-[25px] h-[25px] sm:w-[50px] sm:h-[50px] border-2 border-[#6E9753] rounded-full flex items-center justify-center">
                 {time}
             </div>
 
             <div className={timelineClass}>
-                <time className="text-[18px] font-bold font-[urbanist] pt-[8px] ml-[25px]">Day {time} : {title}</time>
+                <time className="text-[18px] font-bold font-[urbanist] pt-[0px] pl-[10px] sm:pl-[0px]">Day {time} : {title}</time>
                 <ul className="package_detail text-[#4f5e71] text-justify break-words mt-4 font-[urbanist] font-[500] flex flex-col gap-2 text-[16px] leading-[1.5em]"
                     dangerouslySetInnerHTML={{ __html: description }}>
                 </ul>
