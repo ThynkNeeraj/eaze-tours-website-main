84069583ffa5fb5b8cde1f482a39386615079dba
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-03-19 17:43:59 +0530

    Update BlogSummaryCard.tsx

--- components/BlogSummaryCard.tsx
+++ components/BlogSummaryCard.tsx
@@ -15,10 +15,10 @@ function BlogSummaryCard({ blog }: IBlogProp) {
         <>
             <div className="bg-white rounded-[23px] shadow-[0px_0px_2px_1px_#00000040] flex flex-col ml-1">
                 <div
-                    className="w-full h-[210px] aspect-w-2 aspect-h-1 bg-cover bg-center rounded-t-[23px] border-b border-gray-300"
+                    className="w-full h-[160px] sm:h-[210px] aspect-w-2 aspect-h-1 bg-cover bg-center rounded-t-[23px] border-b border-gray-300"
                     style={{ backgroundImage: `url(${blog.image})` }}
                 ></div>
-                <div className="w-full px-4 py-10 flex flex-col justify-center gap-5">
+                <div className="w-full px-4 py-10 flex flex-col justify-center gap-2 sm:gap-5">
                     <div className="flex items-start space-x-2">
                         <p className="text-[16px] text-[#666666BF]">Admin</p>
                         <ul className="list-disc pl-5 space-y-2 text-[16px] text-[#666666BF] marker:text-[#025C7A]">
