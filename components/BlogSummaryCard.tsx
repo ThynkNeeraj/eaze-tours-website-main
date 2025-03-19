6f624d6728050514a14616ced98d50f6010a1964
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-03-19 12:24:31 +0530

    Update BlogSummaryCard.tsx

--- components/BlogSummaryCard.tsx
+++ components/BlogSummaryCard.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { IBlogDataType } from "../types/Common";
 
 interface IBlogProp {
     blog: IBlogDataType;
 }
 
-function Blog({ blog }: IBlogProp) {
+function BlogSummaryCard({ blog }: IBlogProp) {
     const getPackageHref = (Urlb: string) => {
-        return "/blog/" + Urlb
-    }
+        return "/blog/" + Urlb;
+    };
 
     return (
         <>
             <div className="bg-white rounded-[23px] shadow-[0px_0px_2px_1px_#00000040] flex flex-col ml-1">
                 <div
                     className="w-full h-[210px] aspect-w-2 aspect-h-1 bg-cover bg-center rounded-t-[23px] border-b border-gray-300"
-                    style={{ backgroundImage: `url(${blog.Img})` }}
+                    style={{ backgroundImage: `url(${blog.image})` }}
                 ></div>
                 <div className="w-full px-4 py-10 flex flex-col justify-center gap-5">
                     <div className="flex items-start space-x-2">
@@ -26,21 +26,25 @@ function Blog({ blog }: IBlogProp) {
                         </ul>
                     </div>
 
-                    <p className="text-[22px] text-[#04000B] line-clamp-2 font-semibold hover:text-[#6E9753]"><Link href={getPackageHref(blog.Urlb)} passHref>
-                        {blog.Page_heading}</Link></p>
+                    <p className="text-[22px] text-[#04000B] line-clamp-2 font-semibold hover:text-[#6E9753]">
+                        <Link href={getPackageHref(blog.url)} passHref>
+                            {blog.title}
+                        </Link>
+                    </p>
                     <div>
                         <div className="flex items-start space-x-2">
-                            <p className="text-md text-[#04000B]"><Link href={getPackageHref(blog.Urlb)} passHref>
-                                Continue Reading
-                            </Link></p>
-                            <i className="fa fa-arrow-right text-[16px] mt-[5px] text-[#025C7A]" />
+                            <p className="text-md text-[#04000B]">
+                                <Link href={getPackageHref(blog.url)} passHref>
+                                    Continue Reading
+                                </Link>
+                            </p>
+                            <i className="fa fa-arrow-right text-lg text-[#025C7A] -mt-1" />
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-
+    );
 }
 
-export default Blog;
+export default BlogSummaryCard;
